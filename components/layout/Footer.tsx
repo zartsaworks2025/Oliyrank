0242e4c542df7f22cb28d0a162f88c61e86e9814
unknown <ITuser@pc03-50-OA.pb.dom>
2026-01-09 22:52:57 +0500

    footer fixed

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -11,6 +11,7 @@ import {
     FaPhone,
     FaEnvelope,
 } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
@@ -30,7 +31,7 @@ export default function Footer() {
                     </p>
                     <div className="footer-social">
                         <a
-                            href="https://facebook.com"
+                            href="https://facebook.com/share/17tGNvinwR/?mibextid=wwXIfr"
                             target="_blank"
                             rel="noopener noreferrer"
                             aria-label="Facebook"
@@ -38,15 +39,15 @@ export default function Footer() {
                             <FaFacebook />
                         </a>
                         <a
-                            href="https://twitter.com"
+                            href="https://x.com/OliyRank"
                             target="_blank"
                             rel="noopener noreferrer"
                             aria-label="Twitter"
                         >
-                            <FaTwitter />
+                            <FaXTwitter />
                         </a>
                         <a
-                            href="https://instagram.com"
+                            href="https://www.instagram.com/zartsaworks/"
                             target="_blank"
                             rel="noopener noreferrer"
                             aria-label="Instagram"
